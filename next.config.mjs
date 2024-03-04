a2713953b84a362316cb010fc978c4c5bf6a49bd
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-03-04 20:33:00 +0900

    chore : 폰트 파일 불러올 수 있도록 file loader 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(woff2)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "fonts/",
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
