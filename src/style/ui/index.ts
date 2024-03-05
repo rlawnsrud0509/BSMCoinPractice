05ecde5ac3d8fff03d1077c348993e11570a9ec9
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-03-05 23:17:49 +0900

    add : ui컴포넌트 모아서 export

--- src/style/ui/index.ts
+++ src/style/ui/index.ts
@@ -1,1 +1,4 @@
 export { default as Applayout } from "@/style/ui/layout/AppLayout";
+export { default as Row } from "@/style/ui/flex/Row";
+export { default as Column } from "@/style/ui/flex/Column";
+export { default as ImageRenderer } from "@/style/ui/imageRenderer";
