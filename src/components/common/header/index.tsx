fff29e61d8de8474f23a6de9049d1f63f3998360
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-03-05 23:14:23 +0900

    add : 헤더 초안 만듬

---
+++ src/components/common/header/index.tsx
@@ -0,0 +1,89 @@
+import Color from "@/style/color";
+import fonts from "@/style/font";
+import { ImageRenderer, Row } from "@/style/ui";
+import Link from "next/link";
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Container>
+      <Row gap="3rem">
+        <ImageRenderer width="2rem" height="2rem" src="/image/logo/logo.png" />
+        <StyledLink href={"/"}>살펴보기</StyledLink>
+      </Row>
+      <LoginButton href={"/"}>시작하기</LoginButton>
+    </Container>
+  );
+};
+
+export default Header;
+
+const Container = styled.header`
+  width: 100%;
+  height: 4rem;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 1rem;
+
+  background-color: rgba(0, 0, 0, 0);
+  color: ${Color.white};
+
+  backdrop-filter: blur(2px);
+
+  z-index: 5;
+`;
+
+const StyledLink = styled(Link)`
+  width: fit-content;
+  height: 2.5rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 0.5rem;
+  background-color: ${Color.indigoBase};
+
+  transition: 0.25s ease-in-out;
+  cursor: pointer;
+
+  padding: 0 0.5rem;
+
+  ${fonts.Route}
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+`;
+
+const LoginButton = styled(Link)`
+  width: fit-content;
+  height: 2.5rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 0.5rem;
+  background-color: ${Color.white};
+  color: ${Color.black};
+
+  transition: 0.25s ease-in-out;
+  cursor: pointer;
+
+  padding: 0 0.5rem;
+
+  ${fonts.Route}
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+`;
