1fbb52c647c9e800cefe1dc51fff14aff2f9028b
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-03-05 23:15:15 +0900

    add : flex, column ui 컴포넌트 추가

---
+++ src/style/ui/flex/Column/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import { ColumnContainerProptypes } from "@/types/ui/FlexContainerProptypes";
+import fonts from "@/style/font";
+
+type Font = keyof typeof fonts;
+
+const Column = ({
+  width = "fit-content",
+  height = "fit-content",
+  justifyContent = "center",
+  alignItems = "center",
+  gap,
+  fontType = "Footnote",
+  children,
+}: ColumnContainerProptypes) => {
+  return (
+    <Container
+      fontType={fontType}
+      style={{ width, height, justifyContent, alignItems, gap }}
+    >
+      {children}
+    </Container>
+  );
+};
+
+export default Column;
+
+const Container = styled.div<{ fontType: Font }>`
+  display: flex;
+  flex-direction: column;
+
+  ${({ fontType }) => fonts[fontType]};
+`;
---
+++ src/style/ui/flex/Row/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import { ColumnContainerProptypes } from "@/types/ui/FlexContainerProptypes";
+import fonts from "@/style/font";
+
+type Font = keyof typeof fonts;
+
+const Row = ({
+  width = "fit-content",
+  height = "fit-content",
+  justifyContent = "center",
+  alignItems = "center",
+  gap = "0",
+  fontType = "Footnote",
+  children,
+}: ColumnContainerProptypes) => {
+  return (
+    <Container
+      fontType={fontType}
+      style={{ width, height, justifyContent, alignItems, gap }}
+    >
+      {children}
+    </Container>
+  );
+};
+
+export default Row;
+
+const Container = styled.div<{ fontType: Font }>`
+  display: flex;
+
+  ${({ fontType }) => fonts[fontType]};
+`;
