9c71ffad1a9067e29b0836c4216611ab6e39087b
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-03-20 20:11:58 +0900

    chore: 컴포넌트 폴더 모아서 내보내기

---
+++ client/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as StockGraphBox } from "@/components/stockgraphBox";
+export { default as StockInfoBox } from "@/components/stockinfoBox";
+export { default as StockTradeBox } from "@/components/stockTradeBox";
+export { default as Provider } from "@/components/provider";
