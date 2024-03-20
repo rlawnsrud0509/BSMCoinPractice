306538d3ec7d6b7819984138487242d47ba7bbc0
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-03-20 20:13:48 +0900

    add: 버튼 ui 컴포넌트 추가

--- client/src/style/ui/index.ts
+++ client/src/style/ui/index.ts
@@ -3,3 +3,4 @@ export { default as Row } from "@/style/ui/flex/Row";
 export { default as Column } from "@/style/ui/flex/Column";
 export { default as ImageRenderer } from "@/style/ui/imageRenderer";
 export { default as Text } from "@/style/ui/text";
+export { default as Button } from "@/style/ui/button";
