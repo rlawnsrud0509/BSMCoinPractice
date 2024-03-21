a977e27eea713920499822aa40eb468addec520b
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-03-21 20:21:07 +0900

    add : CrossLine (X) 아이콘 추가

--- client/src/style/svg/index.ts
+++ client/src/style/svg/index.ts
@@ -1,2 +1,3 @@
 export { default as Triangle } from "@/style/svg/triangle";
 export { default as Dashline } from "@/style/svg/dashline";
+export { default as Crossline } from "@/style/svg/crossline";
